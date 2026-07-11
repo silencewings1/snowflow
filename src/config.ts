a77ab9f70cf3bdf240a3600acc3fd55c7ab978ff
ospacer <995375381@qq.com>
2026-07-12 03:05:36 +0800

    update browser tab branding

--- src/config.ts
+++ src/config.ts
@@ -30,12 +30,9 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{ src: "/favicon/snowflow-v1.svg", sizes: "any" },
+		{ src: "/favicon/favicon-light-32.png", sizes: "32x32" },
+		{ src: "/favicon/favicon-light-180.png", sizes: "180x180" },
 	],
 };
 
